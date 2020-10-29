89f032ab7cf22a070cc7209505fde11ad935ecec
engg-ankit2212 <72604990+engg-ankit2212@users.noreply.github.com>
2020-10-29 14:35:37 +0530

    Update python_interpreter.sh

--- python_interpreter.sh
+++ python_interpreter.sh
@@ -31,8 +31,8 @@ export PATH=${PYTHON_LOCAL}/bin:${PATH}
 python3 -VVV
 
 
-echo "print(\"Hello, World!\")" > ${working_dir}/hell_world.py
-echo ${working_dir}
+# echo "print(\"Hello, World!\")" > ${working_dir}/hell_world.py
+# echo ${working_dir}
 
-echo "Running hello world Program"
-python3 ${working_dir}/hell_world.py
+# echo "Running hello world Program"
+# python3 ${working_dir}/hell_world.py
